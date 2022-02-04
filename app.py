d45e7fac819a4cdb613e6e6d1e9354a589ee0c3d
Noeyqun1 <92035147+Noeyqun1@users.noreply.github.com>
2022-02-04 21:56:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'wCTiPkhODPylcCwCXit3jJMmBsoPjTJPDcCbB+bruqMRIlwx7rS8wMjQs+9ntI2Kxb2EgyJuRfUB/AVh1xp7yyfLq1Ghx9h+CQoN31qF76dg1X9JVcxFuqD4KSvSHmLnhVpCLWYxPpqKJZigiDhMhQdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
